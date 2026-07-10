e23292dd65b1e3c153fba938eb3035137ef0263a
Gaurav Gaisenn <gauravgaisenn@gmail.com>
2026-07-10 08:48:02 +0530

    feat(squadNumber.ts): add positional squad numbers configuration

--- lib/scoring/squadNumber.ts
+++ lib/scoring/squadNumber.ts
@@ -9,6 +9,16 @@ export function generateSquadNumber(login: string, position: Position): number {
 
     hash = Math.abs(hash);
 
+    //Iconic squad numbers grouped by football positions
+    const posNumbers: Record<Position, number[]> = {
+        ST: [7, 9, 10, 11, 19, 22, 18, 20, 14, 45, 99],
+        RW: [7, 10, 11, 17, 22, 21, 20, 23, 18, 10, 19],
+        CAM: [10, 8, 11, 20, 22, 18, 23, 42],
+        CM: [8, 6, 14, 16, 17, 22, 16, 15, 19, 20, 88],
+        CDM: [6, 4, 15, 16, 14, 28, 23, 8, 18, 25, 5],
+        CB: [4, 5, 2, 3, 6, 12, 15, 26, 33],
+    };
+
 
     return hash;
 }
